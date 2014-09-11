afa96aea67f14ea5b69a6b5ad6a0c06e6e15c401
Brian Mikolajczyk <brianm12@gmail.com>
2014-09-11 15:56:11 -0400

    Added error calculator; Fixed errcalc for accurate total file size

--- install.sh
+++ install.sh
@@ -1,16 +1,29 @@
+#!/bin/bash
 #Installation File
 #Directory Rescue Crawler, direscraw v1.0
 #Written by Brian Mikolajczyk, brianm12@gmail.com
-#!/bin/bash
-if [ -x $(which ddrescue) ]; then
+if [ ! -x $(which lzip ]; then
+lver=$(wget http://download.savannah.gnu.org/releases/lzip/ -qO - | grep lzip | grep -v plzip | grep tar.gz | grep -v sig | cut -d\" -f8 | tail -1)
+wget -q http://download.savannah.gnu.org/releases/lzip/$lver
+gunzip $lver
+tar xf "${lver%.gz}"
+cd "${lver%.tar.gz}"
+./configure
+make
+sudo make install
+cd ..
+fi
+if [ ! -x $(which ddrescue) ]; then
+ddver=$(wget http://mirrors.kernel.org/gnu/ddrescue/ -qO - | grep -v sig | grep href | tail -1 | cut -d\> -f2 | cut -d\< -f1)
+wget -q http://mirrors.kernel.org/gnu/ddrescue/$ddver
+lzip -d $ddver
+tar xf "${ddver%.lz}"
+cd "${ddver%.tar.lz}"
+./configure
+make
+sudo make install
+cd ..
+fi
 cp direscraw /usr/local/bin
 chmod a+x /usr/local/bin/direscraw
 echo "direscraw installed successfully"
-else
-if [ -x $(which lzip ]; then
-ddver=$(wget http://mirrors.kernel.org/gnu/ddrescue/ -qO - | grep -v sig | grep href | tail -1 | cut -d\> -f2 | cut -d\< -f1)
-wget http://mirrors.kernel.org/gnu/ddrescue/$ddver
-lzip -d $ddver
-else sudo apt-get install lzip -y
-fi
-
