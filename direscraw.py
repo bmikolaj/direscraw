b36486ef3875d1a31d12057e2ee8ffb039562597
Brian Mikolajczyk <brianm12@gmail.com>
2014-09-21 18:30:38 -0400

    Removed old program; Added Runtime to errcalc; Added header to *error_summary

--- direscraw.py
+++ direscraw.py
@@ -30,7 +30,7 @@ def main(input_dir=None, output_dir=None):
                     drclog.flush()
                     in_file_path = os.path.join(current_dir, filename)
                     out_file_path = os.path.join(current_out_dir, filename)
-                    print in_file_path 
+                    print(in_file_path)
                     subprocess.call(["ddrescue", in_file_path, out_file_path],
                                     stdout=drclog)
                 drclog.seek(0)
@@ -39,12 +39,16 @@ def main(input_dir=None, output_dir=None):
             
             with open(os.path.join(current_out_dir, 'error_summary'),
             'w') as error_summary:
+                error_summary.write('File Error% RunTime' + '\n')
+                error_summary.seek(20)
                 subprocess.call(['errcalc', os.path.join(current_out_dir,
                                 'drclog')], stdout=error_summary)
                 os.remove(os.path.join(current_out_dir, 'drclog'))
 
     with open(os.path.join(output_dir, 'full_error_summary'),
     'w') as full_error_summary:
+        full_error_summary.write('File Error% RunTime' + '\n')
+        full_error_summary.seek(20)
         subprocess.call(['errcalc', os.path.join(output_dir, 'fulldrclog')],
                         stdout=full_error_summary)
         os.remove(os.path.join(output_dir, 'fulldrclog'))
