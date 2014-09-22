ac63416684c0dc8b8db31cd6315d463134eca253
Brian Mikolajczyk <brianm12@gmail.com>
2014-09-21 23:12:00 -0400

    Added ddrescue terminal display; Program complete

--- direscraw.py
+++ direscraw.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+#Directory Rescue Crawler, direscraw v1.0
+#Written by Brian Mikolajczyk, brianm12@gmail.com
 
 import os.path
 import argparse
@@ -24,15 +26,13 @@ def main(input_dir=None, output_dir=None):
                 pass
 
             with open(os.path.join(current_out_dir, 'drclog'), 'w+') as drclog:
-                #for filename in filenames:
                 for filename in [f for f in filenames if f not in blacklist]:
                     drclog.write('{}\n'.format(filename))
                     drclog.flush()
                     in_file_path = os.path.join(current_dir, filename)
                     out_file_path = os.path.join(current_out_dir, filename)
                     print(in_file_path)
-                    subprocess.call(["ddrescue", in_file_path, out_file_path],
-                                    stdout=drclog)
+                    subprocess.call(["ddrescue $0 $1 | tee -a drclog", in_file_path, out_file_path], shell=True)
                 drclog.seek(0)
                 for line in drclog:
                     fulldrclog.write(line)
@@ -40,7 +40,7 @@ def main(input_dir=None, output_dir=None):
             with open(os.path.join(current_out_dir, 'error_summary'),
             'w') as error_summary:
                 error_summary.write('File Error% RunTime' + '\n')
-                error_summary.seek(20)
+                error_summary.flush()
                 subprocess.call(['errcalc', os.path.join(current_out_dir,
                                 'drclog')], stdout=error_summary)
                 os.remove(os.path.join(current_out_dir, 'drclog'))
@@ -48,7 +48,7 @@ def main(input_dir=None, output_dir=None):
     with open(os.path.join(output_dir, 'full_error_summary'),
     'w') as full_error_summary:
         full_error_summary.write('File Error% RunTime' + '\n')
-        full_error_summary.seek(20)
+        full_error_summary.flush()
         subprocess.call(['errcalc', os.path.join(output_dir, 'fulldrclog')],
                         stdout=full_error_summary)
         os.remove(os.path.join(output_dir, 'fulldrclog'))
