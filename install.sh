0d09c011c97a071dffc202049dbab8ab273851af
Brian Mikolajczyk <brianm12@gmail.com>
2014-09-22 11:20:02 -0400

    Fixed typo and added fail-safe for installation in install.sh

--- install.sh
+++ install.sh
@@ -2,15 +2,13 @@
 #Installation File
 #Directory Rescue Crawler, direscraw v1.0
 #Written by Brian Mikolajczyk, brianm12@gmail.com
-if [ ! -x $(which lzip ]; then
+if [ ! -x $(which lzip) ]; then
 lver=$(wget http://download.savannah.gnu.org/releases/lzip/ -qO - | grep lzip | grep -v plzip | grep tar.gz | grep -v sig | cut -d\" -f8 | tail -1)
 wget -q http://download.savannah.gnu.org/releases/lzip/$lver
 gunzip $lver
 tar xf "${lver%.gz}"
 cd "${lver%.tar.gz}"
-./configure
-make
-sudo make install
+./configure && make && sudo make install
 cd ..
 fi
 if [ ! -x $(which ddrescue) ]; then
@@ -19,9 +17,7 @@ wget -q http://mirrors.kernel.org/gnu/ddrescue/$ddver
 lzip -d $ddver
 tar xf "${ddver%.lz}"
 cd "${ddver%.tar.lz}"
-./configure
-make
-sudo make install
+./configure && make && sudo make install
 cd ..
 fi
 if [ ! -x $(which units) ]; then
@@ -30,9 +26,7 @@ wget -q http://ftp.gnu.org/gnu/units/$unvar
 gunzip $unvar
 tar xf "${unvar%.gz}"
 cd "${unvar%.tar.gz}"
-./configure
-make
-sudo make install
+./configure && make && sudo make install
 cd ..
 fi
 sudo cp errcalc /usr/local/bin
