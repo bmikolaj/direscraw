adb3ef1200d958c6959e997b24d8cf7b7738fb70
Brian Mikolajczyk <brianm12@gmail.com>
2014-10-27 14:30:07 -0400

    Recombined build.html: split unnecessary; Added pdftest.py: Working with wkhtmltopdf

---
+++ pdftest.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import argparse
+import pdfkit
+import os.path
+
+def main(input=None):
+    input = os.path.abspath(input)
+    input2 = input.rsplit('.', 1)[0] + '2.' + input.rsplit('.', 1)[1]
+    output_dir = os.path.split(input)[0]
+    output = input.rsplit('.', 1)[0] + '.pdf'
+    options = {
+        'quiet': '',
+        'image-dpi': '1000',
+        'image-quality': '100',
+        'disable-smart-shrinking': ''
+        }
+    pdfkit.from_file(input, output, options=options)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input')
+
+    main(**vars(parser.parse_args()))
